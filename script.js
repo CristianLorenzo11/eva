203d3d9a06b4b3e03fc01adbee0ec04315affdb6
cristian emmanuel lorenzo <cristianelprof@gmail.com>
2024-07-26 17:37:33 -0300

    ----
    actualizacion dia 1

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
       const password = loginForm.password.value;
   
       // Verificar si la contraseña es correcta (aquí puedes establecer tu propia lógica)
-      if (password === '32657611') {
+      if (password === '12345') {
         loginContainer.classList.add('hidden');
         content.classList.remove('hidden');
       } else {
